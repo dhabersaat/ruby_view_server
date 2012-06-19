3f7bb52e5c7eba28122e917ecc3342133779a4ba
dhabersaat <donna.habersaat@utexas.edu>
2012-06-18 19:42:40 -0500

    section 2

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,15 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+template = ERB.new "
+
+<% pets = ['cats', 'dogs'] %>
+<% pets.each_with_index do |pet, index| %>
+<p>
+<%= pet %> think they are better than <%= pets[index-1] %> %>
+</p>
+<% end %>
+"
+
+puts template.result(binding)
+
